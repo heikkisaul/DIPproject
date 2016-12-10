e20865824575d3804ea6ac6894a99cf1317cecb4
Heikki Saul <heikki.saul@gmail.com>
2016-12-10 12:22:53 +0200

    added comments and TODOs for easier collaboration

--- object_tracker.py
+++ object_tracker.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # TODO hsv is very noisy, maybe use RGB and a calibration mechanism?
-# TODO increase robustness of primary blob
+# TODO increase robustness of primary blob (currently gives "no blob" on a lot of frames)
 
 
 def threshold(frame,HSV_lower, HSV_upper):
@@ -16,6 +16,8 @@ def threshold(frame,HSV_lower, HSV_upper):
 
 def blob_handling(frame):
 
+    # apply filtering to increase detection robustness
+    # TODO test possible filters for increased robustness
     kernel = np.ones((3,3),np.uint8)
 
     filtered_frame = cv2.morphologyEx(frame,cv2.MORPH_OPEN,kernel)
@@ -25,12 +27,14 @@ def blob_handling(frame):
     params.minThreshold = 200
     params.maxThreshold = 255
 
+    # ignore small blobs
     params.filterByArea = True
     params.minArea = 10
 
     detector = cv2.SimpleBlobDetector_create(params)
     keypoints = detector.detect(filtered_frame)
 
+    # if a blob fitting the parameters is detected, return frame and centre coordinates
     if len(keypoints) != 0:
 
         key_X = int(keypoints[0].pt[0])
@@ -39,6 +43,7 @@ def blob_handling(frame):
         key_coords = (key_X,key_Y)
         return filtered_frame, key_coords
 
+    # if no fitting blob is detected, return frame and empty tuple
     else: return filtered_frame,()
 
 
@@ -53,8 +58,11 @@ if __name__ == '__main__':
     while(True):
         ret, frame = capture.read()
 
+        # converts to HSV for increased color robustness
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+        # applies threshold (currently for a small green jar lid, change values accordingly)
+        # H = H/2 (0-180), S = 0-255, V = 0-255
         mask = threshold(hsv,[85, 20, 0],[100,255,255])
 
         blobim,centre_coord = blob_handling(mask)
@@ -62,6 +70,7 @@ if __name__ == '__main__':
 
         hsv = cv2.bitwise_and(hsv,hsv,mask=blobim)
 
+        # shows object in original frame (masked) and the filtered mask
         cv2.imshow('frame',hsv)
         cv2.imshow('blobs',blobim)
 
