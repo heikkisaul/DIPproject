6d51a0bdd72c96f087793f116af4607a0b8cf249
adamsont <taaviadamson@gmail.com>
2017-01-07 13:01:23 +0200

    * Visualizer stub added

--- src/math.py
+++ src/math.py
@@ -18,26 +18,46 @@ class Calculator:
         self.x_coefs = []
         self.y_coefs = []
 
-        self.history = []
+        self.x_buf = []
+        self.y_buf = []
+        self.time_buf = []
+
+        self.on_coords_calculated = None
 
     def calculate_coord(self, coords):
         if coords is not None:
-            current_time = int(round(time.time() * 1000))
+            current_time = int(round(time.time() * 1000)) - self.start_time
 
             if not self.history_full:
-                self.history.append((current_time, coords[0], coords[1]))
+                self.x_buf.append(coords[0])
+                self.y_buf.append(coords[1])
+                self.time_buf.append(current_time)
+
                 self.current_index += 1
 
                 if self.current_index == self.size:
                     self.history_full = True
             else:
-                self.history.pop(0)
-                self.history.append((current_time, coords[0], coords[1]))
+                self.x_buf.pop(0)
+                self.y_buf.pop(0)
+                self.time_buf.pop(0)
+
+                self.x_buf.append(coords[0])
+                self.y_buf.append(coords[1])
+                self.time_buf.append(current_time)
+
+            self.x_coefs = np.polyfit(self.time_buf, self.x_buf, self.rank)
+            self.y_coefs = np.polyfit(self.time_buf, self.y_buf, self.rank)
+
+            self.on_coords_calculated(coords[0], coords[1])
+
+        else:
+            current_time = int(round(time.time() * 1000)) - self.start_time
 
 
+    def predict(self, current_time):
 
-
-    def predict(self):
+        for coef in self.x_coefs:
 
 
 f = open("../data/data3.txt")
---
+++ src/visualizer.py
@@ -0,0 +1,17 @@
+__author__ = 'Taavi'
+
+class Visualizer:
+
+    def __int__(self, coords_source, size):
+        self.frame = None
+        self.size = size
+
+    def update_frame(self, frame):
+        self.frame = frame
+
+    def display(self, coords):
+        # Joonista kõik ajaloos olevad koordinaadid frame peale
+        # Kuva ring ümber hetkekoordinaadi
+        # Kuva kogu pilt
+        pass
+
