a25a0019824075845feb11608365d2cc1d8d6411
RennoR <renno.raudmae@gmail.com>
2017-01-07 15:06:00 +0200

    http://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/#

--- src/visualizer.py
+++ src/visualizer.py
@@ -10,8 +10,52 @@ class Visualizer:
         self.frame = frame
 
     def display(self, coords):
-        # Joonista kõik ajaloos olevad koordinaadid frame peale
-        # Kuva ring ümber hetkekoordinaadi
-        # Kuva kogu pilt
+
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        coords = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        center = None
+
+        # only proceed if at least one contour was found
+        if len(coords) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(coords, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+            # only proceed if the radius meets a minimum size
+            if radius > 10:
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                           (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+        # update the points queue
+        pts.appendleft(center)
+
+        # loop over the set of tracked points
+        for i in range(1, len(pts)):
+            # if either of the tracked points are None, ignore
+            # them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+
+            # otherwise, compute the thickness of the line and
+            # draw the connecting lines
+            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+        # show the frame to our screen
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
+
         pass
 
