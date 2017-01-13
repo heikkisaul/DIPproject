daa225a2438f2b494f025976114fca89f3abc794
adamsont <taaviadamson@gmail.com>
2017-01-13 12:01:34 +0200

    * Added comments

--- src/main.py
+++ src/main.py
@@ -6,23 +6,28 @@ from tracker import *
 import time
 
 if __name__ == '__main__':
-
+	
+	# Display history size
     COORD_ARRAY_LENGTH = 50
 
     calculator = CoordsCalculator(150)
     tracker = Tracker((30,90,90), (40,220,220))
-
+	
+	# Find a camera and start vide capturing
     capture = cv2.VideoCapture(0)
+	
     start_time = int(round(time.time() * 1000))
 
     coord_array = []
 
     while(True):
+		# Read frame and save timestamp
         ret, frame = capture.read()
         timestamp = int(round(time.time() * 1000)) - start_time
 
         coords = calculator.calculate_coords(tracker.process_frame(frame), timestamp)
-
+		
+		# At the beginning when there is not enough data the calculator returns a couple of None's
         if coords is not None:
             x = int(coords[0])
             y = int(coords[1])
@@ -31,15 +36,19 @@ if __name__ == '__main__':
                 coord_array.pop(0)
 
             coord_array.append((x, y))
-
+			
+			# Draw trajectory
             for coord in coord_array:
                 cv2.circle(frame, coord, 5, (0, 0, 255), -1)
-
+		
+		# Display image with drawn trajectory
         cv2.imshow('elevant', frame)
-
+		
+		# Press 'q' to quit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+	
+	# Finalize capturing
     capture.release()
     cv2.destroyAllWindows()
 
